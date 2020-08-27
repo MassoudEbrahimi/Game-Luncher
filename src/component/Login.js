cb37e9eeaee5b0e74dfa4a085f0e9cbf98815dfd
Massoud-Ac <Massoud.ac71@gmail.com>
2020-08-27 21:18:28 +0430

    commit

--- src/component/Login.js
+++ src/component/Login.js
@@ -18,8 +18,8 @@ class Login extends Component {
         debugger
         return (
             <React.Fragment>
-                <div className="d-flex">
-                    <div className="login-sidebar col-sm-6 col-md-3 col-lg-3 col-12">
+                <div className="d-flex login-content">
+                    <div className="login-sidebar card " >
                         <img src="images/Art.svg" className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|} ArtImg-Top" alt="" />
                         <a className="login-weblink" href="./">
                             <span class="mdi mdi-help"></span>
@@ -28,7 +28,7 @@ class Login extends Component {
                             <img src="images/Everfall.svg" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="" />
 
                         </div>
-                        <div class="login-title"><span >Signed in With Your <br /><strong>EverFall Games</strong> Account</span></div>
+                        {/* <div class="login-title"><span >Signed in With Your <br /><strong>EverFall Games</strong> Account</span></div> */}
                         <form className="login-form">
                             {/* <div>
                                 <div> <label class="label-form-login">Username</label></div>
@@ -56,7 +56,7 @@ class Login extends Component {
                             </div>
                             <div class="user-input-wrp">
                                 <br />
-                                <input type="text" class="inputText" required
+                                <input type="password" class="inputText" required
                                     onChange={e => this.setState({ password: e.target.value })}
                                     value={password} autoComplete={false} />
                                 <span class="floating-label">Password</span>
@@ -72,20 +72,20 @@ class Login extends Component {
                                     <span className="mdi mdi-login-variant" />
                                 </button>
                             </div>
+                            <div className="login-footer">
+                                <div>
+                                    <a href="./"><span>CREATE ACCOUNT</span></a>
+                                </div>
+                                <div>
+                                    <a href="./"><span style={{ float: "left" }}>CANT'T SIGN IN?</span></a>
+                                    <span style={{ float: "right" }}>v1.0001</span>
+                                </div>
+                            </div>
                         </form>
-                        <div className="login-footer">
-                            <div>
-                                <a href="./"><span>CREATE ACCOUNT</span></a>
-                            </div>
-                            <div>
-                                <a href="./"><span style={{ float: "left" }}>CANT'T SIGN IN?</span></a>
-                                <span style={{ float: "right" }}>v1.0001</span>
-                            </div>
-                        </div>
                         <img src="images/Art.svg" className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|} ArtImg-bottom" alt="" />
                     </div>
-                    <div className="login-content col-sm-6 col-md-9 col-lg-9">
-                    </div>
+                    {/* <div className="login-content col-sm-5 col-md-8 col-lg-8">
+                    </div> */}
                 </div>
             </React.Fragment>
         );
