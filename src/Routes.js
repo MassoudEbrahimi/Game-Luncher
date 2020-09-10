8eca4a8cc3d83212bbe59c821c18129f6920f37c
Massoud-Ac <Massoud.ac71@gmail.com>
2020-09-10 20:24:47 +0430

    commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from './component/Login';
 import LB2Games from './pages/LB2';
 import Limbo from './pages/Limbo';
