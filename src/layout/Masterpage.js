ccbebb8dd05f78a1d6bd0400462955c08ecc1f2c
Massoud-Ac <Massoud.ac71@gmail.com>
2020-09-19 14:24:37 +0430

    commit

--- src/layout/Masterpage.js
+++ src/layout/Masterpage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Sidebar from './sidebar'
 import "./Masterpage.css"
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 
 class MasterPage extends Component {
@@ -9,7 +10,16 @@ class MasterPage extends Component {
             <div className="page">
                 <Sidebar />
                 <div className="content">
-                {this.props.children}
+                    <ReactCSSTransitionGroup
+                        transitionAppear={true}
+                        transitionAppearTimeout={600}
+                        transitionEnterTimeout={600}
+                        transitionLeaveTimeout={200}
+                        transitionName={window.location.pathname === '/LB-2' ? 'SlideIn' : window.location.pathname === "/Limbo" ? "SlideIn" : "SlideOut"}
+                    >
+                        {this.props.children}
+                    </ReactCSSTransitionGroup>
+                    {/* {this.props.children} */}
                 </div>
             </div>
         );
